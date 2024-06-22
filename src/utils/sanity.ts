a7209bfbb0e812af9b89fe31e01d855fff80c71c
AdrianHell <altadarsh@gmail.com>
2024-06-22 12:52:53 +0530

    Next  commit

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -18,45 +18,18 @@ export const client = createClient({
 const data = await client.fetch(`count(*)`)
 console.log(`Number of documents: ${data}`)
 
-
-
-
 // uses GROQ to query content: https://www.sanity.io/docs/groq
 export async function getBlogs() {
   const posts = await client.fetch('*[_type == "blog"] {"blogname": name,"slug": slug.current,"PublicationDate": PublicationDate,"imageUrl": image.asset->url,"ContentBody": ContentBody[].children[].text,"Summary": Summary[].children[].text,"FurtherReading": FurtherReading[].children[].text}')
-  console.log(`Name: ${posts}`)
+  //console.log(`Name: ${posts}`)
   return posts
 }
-
 
 export async function getBlog(slug: string) {
   const post = await client.fetch(`*[_type == "blog" && slug == $slug]`, {slug})
   return post
 }
 
-
-
-//schema of my blog.
-
-// {
-
-//   "_updatedAt": "2024-06-01T13:42:09Z",
-//   "name": "Exciting AI Stuff",
-//   "_id": "334d901a-6e61-46b2-9039-ba7a69f17919",
-//   "slug": {
-//     "current": "/first",
-//     "_type": "slug"
-//   },
-//   "image": {
-//     "_type": "image",
-//     "asset": {
-//       "_ref": "image-579dbb84e85f9b8314ed3f06f17723ef1b853729-4545x3028-jpg",
-//       "_type": "reference"
-//     }
-//   },
-//   "PublicationDate": "2024-06-01T13:41:00.000Z",
-//   "_createdAt": "2024-06-01T13:42:09Z"
-// },
 export type Blog = {
   _id: string;
   name: string;
